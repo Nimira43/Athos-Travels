3ba98c106a8fd3758ca15e9c7c7e2340bf0e869c
Nimira43 <n.rabone72@aol.com>
2025-05-07 22:24:43 +0100

    Changed Box Icons for Font Awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        '--midnight': '#2c3e50',
-        '--evergreen': '#1a5d1a',
-        '--sunset': '#e4a851',
-        '--glacial': '#f8f9fa',
-        '--rock': '#5e6472',
+        'midnight': '#2c3e50',
+        'evergreen': '#1a5d1a',
+        'sunset': '#e4a851',
+        'glacial': '#f8f9fa',
+        'rock': '#5e6472',
       },
     },
   },
